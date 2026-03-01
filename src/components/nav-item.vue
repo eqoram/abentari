f837585b19b62ae334a431a1c89e62b4d572c76b
eqoram <eqoram@protonmail.com>
2026-03-01 19:31:57 +0100

    0.1.2-T109

--- src/components/nav-item.vue
+++ src/components/nav-item.vue
@@ -1033,7 +1033,7 @@
           </q-item-label> -->
           <q-item-label style="white-space: normal; word-break: break-word">
             <span>{{ lenvironment + ' - ' }}</span
-            >0.1.1
+            >0.1.2
           </q-item-label>
         </q-item-section>
       </q-item>
